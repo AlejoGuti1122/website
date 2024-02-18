485955a156df0e5c70942b30a2461284e316513e
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2024-02-17 19:21:07 -0500

    first commit

---
+++ src/data/dataSocialNetworks.ts
@@ -0,0 +1,20 @@
+export const dataSocialNetworks = [
+    {
+      id: 1,
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/AlejandroGutierrez380",
+    },
+    {
+      id: 2,
+      name: "Instagram",
+      icon: "instagram",
+      link: "https://www.instagram.com/alejo9528/",
+    },
+    {
+      id: 3,
+      name: "LinkedIn",
+      icon: "linkedin",
+      link: "https://www.linkedin.com/in/alejandro-guti%C3%A9rrez-9a88a6267/",
+    },
+  ];
